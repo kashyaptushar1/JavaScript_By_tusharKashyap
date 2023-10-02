f201566cb5d6af6f249766904aca0f0325a35e49
kashyaptushar1 <127180405+kashyaptushar1@users.noreply.github.com>
2023-10-02 12:49:07 +0530

    Add files via upload

---
+++ Day02/operators.js
@@ -0,0 +1,52 @@
+// console.log(" Arthmetic Operators")
+// var a = 2;
+// var b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(b%a);
+// console.log(a**b);
+// console.log(a--);
+// console.log(a++);
+// console.log(++a);
+// console.log(--a);
+// console.log("Assignment operator");
+// var  a = 5;
+// console.log(a);
+//  a +=a ;
+//  console.log(a);
+//  a -= a;
+//  console.log(a);
+//  a *= a;
+//  console.log(a);
+// a /= a;
+// console.log(a);
+
+// console.log("Comperision operator")
+// let comp1 = 5;
+// let comp2 = 3;
+// let comp3 = "5";
+// let comp4 = "3";
+// console.log(comp1 == comp2);//false
+// console.log(comp1 != comp2);//true
+// console.log(comp1 === comp3);//true
+// console.log(comp1 == comp3);//true
+// console.log(comp3 !== comp4); // true
+
+// console.log("Logic operators");
+// var a = true;
+// var b = false;
+
+// console.log(a && b); // false
+// var a = false;
+// var b = true;
+
+// console.log(a || b); // true
+// var a = true;
+
+// console.log(!a); // false
+
+
+
+alert("hello")
